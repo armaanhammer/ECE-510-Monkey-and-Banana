9c1c914365fed031fb735da039c9cb0fb1c014bc
Matt Fleetwood <32446381+etcyl@users.noreply.github.com>
2018-04-26 20:11:27 -0700

    Create motor_controll.py

---
+++ Code/Vikingbot/motor_controll.py
@@ -0,0 +1,40 @@
+"""
+Basic test program for the Vikingbot.
+"""
+import motor_controller as MC
+import ultrasonic as US
+import RPi.GPIO as GPIO
+import support_funcs as sf
+import motor_controller as MC
+import ultrasonic as US
+import RPi.GPIO as GPIO
+
+#DO setup here
+#Set GPIO mode to BCM
+GPIO.setmode(GPIO.BCM)
+
+#Create objects for Vikingbot motor controller and HC-SR04 sensor
+vb_motor = MC.MotorController()
+HC_sensor = US.Ultrasonic()
+HC_sensor.setup_GPIO()
+
+vb_motor.setup_GPIO(1,0)
+# setup and start PWM set the dulty cycles to 90
+vb_motor.setup_PWM()
+vb_motor.start_PWM()
+vb_motor.set_motorSpeed(90,90)
+
+# set the delay between motions to 2 seconds
+vb_motor.set_SleepTime(2)
+
+GPIO.setmode(GPIO.BCM)
+HC_sensor.setup_GPIO()
+vb_motor.setup_GPIO(1,0)
+#obj_front = HC_sensor.get_distance()
+#vb_motor.goBack()
+vb_motor.set_SleepTime(1)
+vb_motor.goForward()
+vb_motor.set_SleepTime(1)
+vb_motor.goForward()
+vb_motor.set_SleepTime(1)
+GPIO.cleanup()
