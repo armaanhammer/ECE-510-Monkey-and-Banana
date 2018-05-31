087632724888809efa35d9f8696c996dfda63be1
dherna2 <dhern2@pdx.edu>
2018-05-30 19:07:28 -0700

    Needs python 3 to run
    couldnt further debug due to using python 2.7.6

---
+++ Code/4-wheeled-claw/PMOD_adapter/examples/client_if.py
@@ -0,0 +1,72 @@
+import argparse
+import socket
+
+# import object detection code
+import object_detection as od
+
+def get_args():
+    parser = argparse.ArgumentParser(description='claw client.')
+
+    parser.add_argument(
+            '--host',
+            default=None,
+            help='host ip of the claw')
+
+    parser.add_argument(
+            '--port',
+            default=5555,
+            type=int,
+            help='port used to communicate to the claw')
+
+    parser.add_argument(
+            '-s',
+            '--script',
+            default=None,
+            help='script file to run claw')
+
+    parser.add_argument(
+            '-v',
+            '--vision',
+            action="store_true",
+            help='use OpenCV vision to move the claw bot')
+
+    return parser.parse_args()
+
+def Main(host, port, script, vision):
+        mySocket = socket.socket()
+        mySocket.connect((host,port))
+
+        if vision:
+            od.Main(mySocket=mySocket)
+
+        elif script:
+            with open(script) as f:
+                for line in f:
+                    mySocket.send(line.strip().encode())
+                    data = mySocket.recv(1024).decode()
+
+                    print('Recieved from server: ' + data)
+
+        else:
+            message = input(" -> ")
+
+            while message != 'q':
+                if message:
+                    mySocket.send(message.encode())
+                    data = mySocket.recv(1024).decode()
+
+                    print ('Received from server: ' + data)
+
+                message = input(" -> ")
+
+        mySocket.close()
+
+if __name__ == '__main__':
+    args = get_args()
+
+    print('Host: {}'.format(args.host))
+    print('Port: {}'.format(args.port))
+    print('Script: {}'.format(args.script))
+    print('Vision: {}'.format(args.vision))
+
+    Main(args.host, args.port, args.script, args.vision)
---
+++ Code/4-wheeled-claw/PMOD_adapter/examples/server_if.py
@@ -0,0 +1,171 @@
+import argparse
+import socket
+import time
+#import yaml
+
+from ClawRobot import ClawRobot
+ 
+def get_args():
+    parser = argparse.ArgumentParser(description='clawrobot server.')
+
+    parser.add_argument(
+            '--host',
+            default='192.168.0.104',
+            help='host ip of the clawrobot')
+
+    parser.add_argument(
+            '--port',
+            default=5000,
+            type=int,
+            help='port used to communicate to the clawrobot')
+
+    # parser.add_argument(
+            # '-c',
+            # '--config_file',
+            # default='config_12DOF.yaml',
+            # help='clawrobot configuration file')
+    
+    return parser.parse_args()
+
+#def initialize_clawrobot(config_file):
+def initialize_clawrobot():
+    my_clawrobot = ClawRobot(motors1_port='JC', motors2_port='JB', servos_port='JAB')
+    
+    # # open config file
+    # with open(config_file) as f:
+        # my_config = yaml.load(f)
+
+    # if config_file == 'config_12DOF.yaml':
+        # my_clawrobot = clawrobot_12DOF(my_config)
+        # my_clawrobot.initial_tests()
+        
+    # elif config_file == 'config_18DOF.yaml':
+        # my_clawrobot = clawrobot_18DOF(my_config)
+        # my_clawrobot.initial_tests()
+        
+    # else:
+        # print('ERROR: Cannot recognize the config file!!!')
+
+    return my_clawrobot
+
+
+def command_processor(data, my_clawrobot):
+    print('Data: {}'.format(data))
+
+    items = data.split()
+    if len(items) == 1:
+        command = items[0]
+        iteration = 1
+
+    elif len(items) == 2:
+        command = items[0]
+        iteration = int(items[1])
+
+    else:
+        return 'ERROR: invalid command format'
+
+    print('Command: {}'.format(command))
+    print('Iteration: {}'.format(iteration))
+
+    commands = [
+        'open',
+        'close',
+        'left',
+        'right',
+        'move_forward',
+        'go'
+        'move_backward',
+        'reverse',
+    ]
+    
+    if command == 'open':
+        print('Opening claw...')
+        my_clawrobot.open_claw()
+        return ('Opened claw' )
+
+    elif command == 'close':
+        print('Closing claw...')
+        my_clawrobot.close_claw()
+        return ('Closed claw')
+
+    elif command == 'left':
+        for i in range(iteration):
+            print('turning left...')
+            my_clawrobot.left(delay=2, power=50)
+
+        return ('Turning Left {} times'.format(iteration))
+
+    elif command == 'right':
+        for i in range(iteration):
+            print('turning right...')
+            my_clawrobot.right(delay=2, power=50)
+
+        return ('Turning Right {} times'.format(iteration))
+
+    elif command == 'move_forward' or command == 'go':
+        for i in range(iteration):
+            print('moving forwards...')
+            my_clawrobot.forward(delay=2, power=50)
+
+        return ('Moved Forward {} times'.format(iteration))
+
+    elif command == 'move_backward' or command == 'reverse':
+        for i in range(iteration):
+            print('moving backwards...')
+            my_clawrobot.reverse(delay=2, power=50)
+
+        return ('Moved Backward {} times'.format(iteration))
+
+    elif command == 'commands':
+        temp = 'Implemented Commands: '
+        for command in commands:
+            temp += command + ', '
+
+        return temp
+
+    else:
+        print('ERROR: command not recognized: {}'.format(command))
+        return 'Command not found!'
+
+def listenToClient(client, address, my_clawrobot):
+    size=1024
+    while True:
+        try:
+            data = client.recv(size).decode()
+            if data:
+                # Read command from client
+                print("from connected user: " + str(data))
+                response = command_processor(data, my_clawrobot)
+
+                # data = str(data).upper()
+                client.send(response.encode())
+            else:
+                raise error('Client disconnected')
+        except:
+            client.close()
+            return False
+
+#def Main(host, port, config_file):
+def Main(host, port):
+    #my_clawrobot = initialize_clawrobot(config_file)
+    my_clawrobot = initialize_clawrobot()
+
+    mySocket = socket.socket()
+    mySocket.bind((host,port))
+     
+    mySocket.listen(5)
+    while True:
+        client, address = mySocket.accept()
+        client.settimeout(300)
+        print ("Connection from: " + str(address))
+        listenToClient(client, address, my_clawrobot)
+     
+if __name__ == '__main__':
+    args = get_args()
+
+    print('Host: {}'.format(args.host))
+    print('Port: {}'.format(args.port))
+    #print('Config File: {}'.format(args.config_file))
+
+    #Main(args.host, args.port, args.config_file)
+    Main(args.host, args.port)
