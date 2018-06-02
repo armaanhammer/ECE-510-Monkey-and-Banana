9ee492b13a4d73ed4db9c35cc1f2bf4a66416a3b
Matt Fleetwood <32446381+etcyl@users.noreply.github.com>
2018-06-02 14:53:57 -0700

    Update calc.py

--- Code/GameLogic/calc.py
+++ Code/GameLogic/calc.py
@@ -20,18 +20,22 @@ coordinates
 import math
 
 def getAngleAndDist(robot_info, goal_info):
+    """
+    Example call: robo_ls = [90, 5, 5], goal_ls = [180, 0, 0]
+                  (ang, dist) = getAngleAndDist(robo_ls, goal_ls)
+                  
+    This means ang (the difference between 180 and 90) is 90, 
+    and dist (the distance between the pair of (x, y) coords) is 7.07
+    
+    """
 	#Error check to see if we are missing information
-	if(len(robot_info) == 3 or len(goal_info) == 3):
-		print('Error: not enough information or info format not as expected')
-		return
-	
-	angle_diff = abs(robot_info[0] - goal_info[0]) #Find difference between robot and goal angles
-	x_diff = goal_info[1] - robot_info[1] #Get the differences between x and y coordinates
-	y_diff = goal_info[2] - robot_info[2]
-	x_sq = math.pow(x_diff, 2) #Apply the rest of the distance formula
-	y_sq = math.pow(y_diff, 2)
-	x_y_diff = x_sq + y_sq
-	return (angle_diff, math.sqrt(x_y_diff)) 
-	
-	
-
+    if(len(robot_info) != 3 or len(goal_info) != 3):
+        print('Error: not enough information or info format not as expected')
+        return
+    angle_diff = goal_info[0] - robot_info[0] #Find difference between robot and goal angles
+    x_diff = abs(goal_info[1] - robot_info[1]) #Get the differences between x and y coordinates
+    y_diff = abs(goal_info[2] - robot_info[2])
+    x_sq = math.pow(x_diff, 2) #Apply the rest of the distance formula
+    y_sq = math.pow(y_diff, 2)
+    x_y_diff = x_sq + y_sq
+    return (angle_diff, math.sqrt(x_y_diff)) 
