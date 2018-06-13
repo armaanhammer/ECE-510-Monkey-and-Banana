1a090c742d012e8eda5bf8995f76ac28ea285967
Armaan Roshani <armaan@gmail.com>
2018-06-12 22:20:07 -0700

    larger images
    manual images

--- Code/Aruco/generate_id_boards/print_aruco_numbers.py
+++ Code/Aruco/generate_id_boards/print_aruco_numbers.py
@@ -2,6 +2,7 @@ import cv2
 import cv2.aruco as aruco
 from random import randint
 
+manual_nums = [880,481,818,58,209,839]
 
 size = 200
 num_markers = 5
@@ -9,19 +10,27 @@ ids = []
 
 dictionary = aruco.getPredefinedDictionary(aruco.DICT_ARUCO_ORIGINAL)
 
-for i in range(num_markers):
-    id_not_unique = True
-    id = None
-    while id_not_unique:
-        id = randint(1, len(dictionary.bytesList) + 1)
-        if id not in ids:
-            id_not_unique = False
-            ids.append(id)
-            
-    img = aruco.drawMarker(dictionary, id, size)
-    img_name = 'aruco_{}.png'.format(id)
+for i in (manual_nums):
+    img = aruco.drawMarker(dictionary, i, size)
+    img_name = 'aruco_{}.png'.format(i)
     cv2.imwrite(img_name, img)
     
+
+
+# for i in range(num_markers):
+#     id_not_unique = True
+#     id = None
+#     while id_not_unique:
+#         id = randint(1, len(dictionary.bytesList) + 1)
+#         if id not in ids:
+#             id_not_unique = False
+#             ids.append(id)
+            
+#     img = aruco.drawMarker(dictionary, id, size)
+#     img_name = 'aruco_{}.png'.format(id)
+#     cv2.imwrite(img_name, img)
+
+#################################
     
 # img = aruco.drawMarker(dictionary, 1, 100)
 # cv2.imwrite('aruco_1.png', img)
