b0f26b0d0fb47c2cf6475a64e1bc4ba988dff7d5
Matt Fleetwood <32446381+etcyl@users.noreply.github.com>
2018-06-14 06:32:22 -0700

    Update phase2.pl

--- Code/GameLogic/master/phase2.pl
+++ Code/GameLogic/master/phase2.pl
@@ -11,6 +11,17 @@
 	bottom corners).
 */
 
+robot_is_at_center(RobotX, RobotY, RampTopLeftCornerX, RampBotLeftCornerX, RampTopRightCornerX, RampBotRightCornerX, 
+					RampTopLeftCornerY, RampBotLeftCornerY, RampTopRightCornerY, RampBotRightCornerY) :-
+		RobotX > RampBotLeftCornerX,
+		RobotX > RampTopLeftCornerX, 
+		RobotX < RampBotRightCornerX,
+		RobotX < RampTopRightCornerX,
+		RobotY > RampBotLeftCornerY,
+		RobotY > RampBotRightCornerY,
+		RobotY < RampTopLeftCornerY,
+		RobotY < RampTopRightCornerY.
+
 /* Robot is behind the ramp if the robot's Y-postion is greater than both the top left and right corners of the ramp */
 robot_is_behind_ramp(RobotY, RampTopLeftCornerY, RampTopRightCornerY) :-
 		RobotY > RampLeftCornerY,
@@ -40,3 +51,24 @@ rotate_270_degrees_east_of_ramp(RobotX, RobotY, RampTopLeftCornerX, RampBotLeftC
 rotate_90_degrees_west_of_ramp(RobotX, RobotY, RampTopLeftCornerY, RampTopRightCornerY, RampTopRightCornerX, RampBotRightCornerX) :-
 		robot_is_behind_ramp(RobotY, RampTopLeftCornerY, RampTopRightCornerY),
 		robot_is_right_of_ramp(RobotX, RampTopRightCornerX, RampBotRightCornerX.
+
+/* IF the robot's (x, y) position is NOT in front of the ramp, THEN rotate 180 degrees south of the ramp*/		
+rotate_180_degrees_south_of_ramp(RobotY, RampBotLeftCornerY, RampBotRightCornerY) :-
+		not(robot_is_in_front_of_ramp(RobotY, RampBotLeftCornerY, RampBotRightCornerY)).
+
+/* IF robot's (x, y) position is in front of the ramp AND to the left of the ramp, THEN rotate 90 degrees of the ramp and move to the center of the ramp */
+rotate_90_degrees_and_move_to_center(RobotX, RobotY, RampTopLeftCornerX, RampBotLeftCornerX, RampBotLeftCornerY, RampBotRightCornerY) :-
+		robot_is_in_front_of_ramp(RobotY, RampBotLeftCornerY, RampBotRightCornerY),
+		robot_is_left_of_ramp(RobotX, RampTopLeftCornerX, RampBotLeftCornerX).
+
+/* IF robot's (x, y) position is in front of the ramp AND to the right of the ramp, THEN rotate 270 degrees of the ramp and move to the center of the ramp*/		
+rotate_270_degrees_and_move_to_center(RobotX, RobotY, RampBotLeftCornerY, RampBotRightCornerY, RampTopRightCornerX, RampBotRightCornerX) :-
+		robot_is_in_front_of_ramp(RobotY, RampBotLeftCornerY, RampBotRightCornerY),
+		robot_is_right_of_ramp(RobotX, RampTopRightCornerX, RampBotRightCornerX).
+
+/* IF robot's (x, y) position is in front of the ramp AND in the center, THEN rotate 0 degrees of the ramp and retrieve the can */		
+rotate_0_degrees_and_retrieve_can(RobotX, RobotY, RampTopLeftCornerX, RampBotLeftCornerX, RampTopRightCornerX, RampBotRightCornerX, 
+					RampTopLeftCornerY, RampBotLeftCornerY, RampTopRightCornerY, RampBotRightCornerY ) :-
+		robot_is_in_front_of_ramp(RobotY, RampBotLeftCornerY, RampBotRightCornerY),
+		robot_is_at_center(RobotX, RobotY, RampTopLeftCornerX, RampBotLeftCornerX, RampTopRightCornerX, RampBotRightCornerX, 
+					RampTopLeftCornerY, RampBotLeftCornerY, RampTopRightCornerY, RampBotRightCornerY).
