a040ebd6db722c760e4b7904c17b2dec58b5244d
Matt Fleetwood <32446381+etcyl@users.noreply.github.com>
2018-06-14 06:03:33 -0700

    Update phase2.pl

--- Code/GameLogic/master/phase2.pl
+++ Code/GameLogic/master/phase2.pl
@@ -5,9 +5,9 @@
 	6/14/2018
 	
 	This program uses Prolog rules for Python to determine where the robot should go next. Inputs from Python are (x, y) 
-  coordinates of the robot and the Cartesian coordinates for the left, right, front, and back of the ramp.
+	coordinates of the robot and the Cartesian coordinates for the left, right, front, and back of the ramp.
 	Coordinates are defined by the Aruco markers. The robot uses one marker while the ramp uses four (two for the top and
-  bottom corners).
+	bottom corners).
 */
 
 robot_is_behind_ramp(RobotY, RampTopLeftCornerY, RampTopRightCornerY) :-
